5a642590cdde6e542cb57c5c5e34a4818a42be80
ZohaibR30 <Zohaibrasool30@yahoo.com>
2022-07-24 22:53:58 +0500

    auth

---
+++ authentication and authorization/routes/users.js
@@ -0,0 +1,38 @@
+const {User, validate} = require('../models/user');
+const mongoose = require('mongoose');
+const express = require('express');
+const _ = require('lodash');
+const bcrypt = require('bcrypt');
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    const {error} = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+
+    let user = await User.findOne({ email: req.body.email });
+    if (user) return res.status(400).send('User Already Exist');
+
+    // user = new User({
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     password: req.body.password
+    // });
+
+    // await user.save();
+
+    // res.send({
+    //     name: user.name,
+    //     email: user.email
+    // });
+
+    user = new User(_.pick(req.body, ['name', 'email', 'password']));
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(user.password, salt);
+
+    await user.save();
+    
+    res.send(_.pick(user, ['_id', 'name', 'email']));
+});
+
+module.exports = router;
